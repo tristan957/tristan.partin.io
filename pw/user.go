eaa7f6f945d4e2e48ef17536c93fd3c28a549c49
Tristan Partin <tristan.partin@gmail.com>
2019-03-19 16:25:00 -0500

    Add Resume url

--- pw/user.go
+++ pw/user.go
@@ -4,6 +4,7 @@ package pw
 type User struct {
 	Name      *string      `yaml:"name"`
 	About     *string      `yaml:"about"`
+	ResumeURL *string      `yaml:"resume-url"`
 	Jobs      []*Job       `yaml:"jobs"`
 	Skills    []*Skill     `yaml:"skills"`
 	Schooling []*Education `yaml:"schooling"`
