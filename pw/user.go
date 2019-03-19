fe95857b418f24c2f358a5b74cd53df249da9a9f
Tristan Partin <tristan.partin@gmail.com>
2019-03-19 16:52:16 -0500

    Add picture and resume location

--- pw/user.go
+++ pw/user.go
@@ -2,10 +2,11 @@ package pw
 
 // User contains fields that pertain to a user
 type User struct {
-	Name      *string      `yaml:"name"`
-	About     *string      `yaml:"about"`
-	ResumeURL *string      `yaml:"resume-url"`
-	Jobs      []*Job       `yaml:"jobs"`
-	Skills    []*Skill     `yaml:"skills"`
-	Schooling []*Education `yaml:"schooling"`
+	Name            *string      `yaml:"name"`
+	About           *string      `yaml:"about"`
+	ResumeLocation  *string      `yaml:"resume-location"`
+	PictureLocation *string      `yaml:"picture-location"`
+	Jobs            []*Job       `yaml:"jobs"`
+	Skills          []*Skill     `yaml:"skills"`
+	Schooling       []*Education `yaml:"schooling"`
 }
