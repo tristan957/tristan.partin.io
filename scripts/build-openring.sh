5563519480ab1ec9ef0cf056232164c1f0192031
Tristan Partin <tristan@partin.io>
2020-07-01 02:16:29 -0500

    Use openring as a submodule

---
+++ scripts/build-openring.sh
@@ -0,0 +1,5 @@
+#!/bin/env sh
+
+cd openring
+go get
+go build
---
+++ scripts/generate-openring.sh
@@ -0,0 +1,10 @@
+#!/bin/env sh
+
+./openring/openring \
+	-s https://drewdevault.com/feed.xml \
+	-s https://emersion.fr/blog/rss.xml \
+	-s https://blog.golang.org/feed.atom \
+	-s https://blog.rust-lang.org/feed.xml \
+	-s https://blog.mozilla.org/feed/ \
+	< include/webring-in.html \
+	> layouts/partials/webring-out.html
