4b6eb46907ab46525a094412fe05837fa898c027
Tristan Partin <tristan@partin.io>
2021-03-13 19:15:28 -0600

    Switch to using a webring.txt file for managing feeds

--- scripts/openring/generate.sh
+++ scripts/openring/generate.sh
@@ -1,10 +1,6 @@
 #!/usr/bin/env sh
 
 ./openring/openring \
-	-s https://drewdevault.com/feed.xml \
-	-s https://emersion.fr/blog/rss.xml \
-	-s https://blog.golang.org/feed.atom \
-	-s https://blog.rust-lang.org/feed.xml \
-	-s https://blog.mozilla.org/feed/ \
+	-S webring.txt \
 	< include/webring-in.html \
 	> layouts/partials/webring-out.html
