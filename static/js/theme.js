ba1c23685e6ae5c7990317ff873afc1db246fb4e
Tristan Partin <tristan@partin.io>
2021-05-08 18:01:38 -0500

    Move JS out of line to avoid 'unsafe-inline' in prod

--- static/js/theme.js
+++ static/js/theme.js
@@ -1,39 +1,26 @@
 function isDarkModeEnabled() {
-  const siteThemeFromStorage = window.localStorage.getItem("site-theme");
+  const siteTheme = window.localStorage.getItem("site-theme");
   return (
     (window.matchMedia &&
       window.matchMedia("(prefers-color-scheme: dark)").matches &&
-      siteThemeFromStorage !== "light") ||
-    siteThemeFromStorage === "dark"
+      siteTheme !== "light") ||
+    siteTheme === "dark"
   );
 }
 
-function switchTheme(theme) {
-  if (theme === "dark") {
+function switchTheme(dark) {
+  if (dark) {
     document.documentElement.setAttribute("site-theme", "dark");
     window.localStorage.setItem("site-theme", "dark");
+    darkModePreferred = true;
   } else {
     document.documentElement.setAttribute("site-theme", "light");
     window.localStorage.setItem("site-theme", "light");
-  }
-}
-
-function overrideTheme(button) {
-  if (!darkModePreferred) {
-    darkModePreferred = true;
-    button.innerHTML = "Light Theme";
-    switchTheme("dark");
-  } else {
     darkModePreferred = false;
-    button.innerHTML = "Dark Theme";
-    switchTheme("light");
   }
 }
 
-let darkModePreferred = isDarkModeEnabled();
-
-switchTheme(darkModePreferred ? "dark" : "light");
+switchTheme(isDarkModeEnabled());
 
 const themeButton = document.getElementById("theme-btn");
-themeButton.innerHTML = darkModePreferred ? "Light Theme" : "Dark Theme";
-themeButton.addEventListener("click", (ev) => overrideTheme(ev.target));
+themeButton.addEventListener("click", () => switchTheme(!isDarkModeEnabled()));
