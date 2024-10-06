27c8f6af80098c1cc9e34ce8694922ad2da5d747
Tristan Partin <tristan@partin.io>
2024-10-06 19:30:15 -0400

    Switch to onedark instead of monokai for code blocks
    Signed-off-by: Tristan Partin <tristan@partin.io>

--- scripts/gen-chromastyles.sh
+++ scripts/gen-chromastyles.sh
@@ -3,5 +3,5 @@
 # SPDX-License-Identifier: CC0-1.0
 # SPDX-FileCopyrightText: Tristan Partin <tristan@partin.io>
 
-hugo gen chromastyles --style=monokai >assets/css/syntax-dark.css
-hugo gen chromastyles --style=pygments >assets/css/syntax-light.css
+hugo gen chromastyles --style=onedark >assets/css/syntax-dark.css
+hugo gen chromastyles --style=github >assets/css/syntax-light.css
